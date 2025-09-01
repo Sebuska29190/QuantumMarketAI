7741344dcb5abc7362632e71a8500721de7d74d4
Sebuska29190 <57123390+Sebuska29190@users.noreply.github.com>
2025-09-01 20:52:35 +0200

    Update NewsList.tsx

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NewsModel from '../models/news';
 import New from './New';
+import { Grid } from '@mui/material';
 
 interface Props {
     news: NewsModel[];
@@ -9,9 +10,16 @@ interface Props {
 function NewsList({ news }: Props) {
     return (
         <React.Fragment>
-            {
-                news.map(newz => <New key={newz.id} newz={newz} />)
-            }
+            {/* Używamy komponentu Grid z odpowiednimi odstępami */}
+            <Grid container spacing={3}>
+                {
+                    news.map(newz => (
+                        <Grid item key={newz.id} xs={12} sm={6} md={4}>
+                            <New newz={newz} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
         </React.Fragment>
     );
 }
