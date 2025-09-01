0493767e775379670ea95803bcaaaa86a5970bf4
Sebuska29190 <57123390+Sebuska29190@users.noreply.github.com>
2025-09-01 20:24:36 +0200

    Update NewsList.tsx

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -3,17 +3,17 @@ import NewsModel from '../models/news';
 import New from './New';
 
 interface Props {
-    news: NewsModel[];
+    news: NewsModel[]; // Prawidłowy typ dla tablicy wiadomości
 }
 
 function NewsList({ news }: Props) {
-    return (
-        <React.Fragment>
-            {
-                news.map(newz => <New key={newz._id} newz={newz} />)
-            }
-        </React.Fragment>
-    );
+    return (
+        <React.Fragment>
+            {
+                news.map(newz => <New key={newz._id} newz={newz} />)
+            }
+        </React.Fragment>
+    );
 }
 
 export default NewsList;
