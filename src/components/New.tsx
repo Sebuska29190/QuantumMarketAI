6aeec54a75497e1bde0d45c80f8dadf2e260cf2e
Sebuska29190 <57123390+Sebuska29190@users.noreply.github.com>
2025-09-01 20:33:36 +0200

    Update New.tsx

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -2,13 +2,11 @@ import { Box, Card, CardContent, CardMedia, Grid, Typography } from '@mui/materi
 import React from 'react';
 import NewsModel from '../models/news';
 
-// Definiujemy interfejs Props, aby komponent wiedział, jakiego typu danych oczekuje
 interface Props {
     newz: NewsModel;
 }
 
 function New({ newz }: Props) {
-    // ZABEZPIECZONY KOD DO TWORZENIA DATY
     let publishedDate;
     if (newz.published_date) {
         const date = new Date(newz.published_date);
@@ -34,13 +32,17 @@ function New({ newz }: Props) {
                             {newz.title}
                         </a>
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {newz.summary}
-                    </Typography>
-                    <Box sx={{ mt: 2 }}>
-                        <Typography variant="caption" display="block" gutterBottom>
-                            Published by: {newz.rights}
+                    {newz.summary && ( // Warunek, który renderuje opis tylko, gdy istnieje
+                        <Typography variant="body2" color="text.secondary">
+                            {newz.summary}
                         </Typography>
+                    )}
+                    <Box sx={{ mt: 2 }}>
+                        {newz.rights && ( // Warunek, który renderuje wydawcę tylko, gdy istnieje
+                            <Typography variant="caption" display="block" gutterBottom>
+                                Published by: {newz.rights}
+                            </Typography>
+                        )}
                         {publishedDate && (
                             <Typography variant="caption" display="block" gutterBottom>
                                 Published on: {publishedDate.toLocaleDateString()}
